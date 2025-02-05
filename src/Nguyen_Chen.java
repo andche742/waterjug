03cffce908439bf3e5cb65f1bbb03b83e289f56f
andche742 <andche742@gmail.com>
2025-02-05 01:32:46 -0800

    Initial Commit

---
+++ src/Nguyen_Chen.java
@@ -0,0 +1,99 @@
+import java.util.*;
+
+public class Nguyen_Chen {
+
+    static int[] capacities;
+    static List<List<State>> steps = new ArrayList<>();
+    static Set<State> visited = new HashSet<>();
+    static Map<State, Integer> parentCounts = new HashMap<>();
+
+    public static void main(String[] args) {
+        capacities = new int[]{3, 5, 19};
+        State initialState = new State(0, 0, 19);
+        System.out.println("Capacity: {'Jug A': " + capacities[0] + ", 'Jug B': " + capacities[1] + ", 'Jug C': " + capacities[2] + "}.");
+        System.out.println("Initial amount of water: {'Jug A': " + initialState.a + ", 'Jug B': " + initialState.b + ", 'Jug C': " + initialState.c + "}.");
+
+        bfs(initialState);
+        printStates();  
+    }
+
+    static void bfs(State initialState) {
+        Queue<State> queue = new LinkedList<>();
+        queue.add(initialState);
+        visited.add(initialState);
+        steps.add(List.of(initialState));
+
+        while (!queue.isEmpty()) {
+            HashMap<State, Integer> currentLevel = new HashMap<State, Integer>();
+
+            for (int i = 0, size = queue.size(); i < size; i++) {
+                State current = queue.poll();
+
+                for (int from = 0; from < 3; from++) {
+                    for (int to = 0; to < 3; to++) {
+                        if (from != to) {
+                            State next = pour(current, from, to);
+                            if (next != null && !visited.contains(next)) {
+                                currentLevel.put(next, currentLevel.getOrDefault(next, 0) + 1);
+                                queue.add(next);
+                            }
+                        }
+                    }
+                }
+            }
+
+            if (!currentLevel.isEmpty()) {
+                steps.add(new ArrayList<State>(currentLevel.keySet()));
+                visited.addAll(currentLevel.keySet());
+                parentCounts.putAll(currentLevel);
+            }
+        }
+    }
+
+    static State pour(State current, int from, int to) {
+        int[] amounts = {current.a, current.b, current.c};
+        if (amounts[from] == 0 || amounts[to] == capacities[to]) return null;
+
+        int pourAmount = Math.min(amounts[from], capacities[to] - amounts[to]);
+        amounts[from] -= pourAmount;
+        amounts[to] += pourAmount;
+
+        return new State(amounts[0], amounts[1], amounts[2]);
+    }
+
+    static void printStates() {
+        System.out.println("Step: New Measurements: New States");
+        System.out.println("-------------------------------------------------------------");
+
+        Set<Integer> seenMeasurements = new HashSet<>();
+
+        for (int i = 0; i < steps.size(); i++) {
+            List<State> currentStep = steps.get(i);
+            Set<Integer> newMeasurements = new TreeSet<>();
+            StringBuilder newStates = new StringBuilder();
+
+            for (State s : currentStep) {
+                if (!seenMeasurements.contains(s.a)) newMeasurements.add(s.a);
+                if (!seenMeasurements.contains(s.b)) newMeasurements.add(s.b);
+                if (!seenMeasurements.contains(s.c)) newMeasurements.add(s.c);
+                
+                int parentCount = parentCounts.getOrDefault(s, 1);
+                String parentLabel = "";
+                if (parentCount > 1) {
+                     parentLabel = " p" + parentCount;
+                }
+                newStates.append(s).append(parentLabel).append(", ");
+            }
+
+            if (newStates.length() > 2) {
+                newStates.setLength(newStates.length() - 2);
+            }
+
+            seenMeasurements.addAll(newMeasurements);
+
+            System.out.printf("%-4d: %-15s : %s\n", i, newMeasurements.toString().replaceAll("[\\[\\]]", ""), newStates);
+        }
+
+        System.out.println("-------------------------------------------------------------");
+    }
+}
---
+++ src/State.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class State {
+    int a, b, c;
+    
+    State(int a, int b, int c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        State state = (State) o;
+        return this.a == state.a && this.b == state.b && this.c == state.c;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(a, b, c);
+    }
+
+    @Override
+    public String toString() {
+        return "[" + a + ", " + b + ", " + c + "]";
+    }
+}
